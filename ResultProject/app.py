4ef1b9d74c3a155ad65bf2c6e532d93097bf9756
EsaulVa <esaul_va@mail.ru>
2021-11-11 01:17:40 +0300

    Add files via upload
    Изменения в логике фронтэнда

--- ResultProject/app.py
+++ ResultProject/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
+@app.route('/index')
+def main_index():
+    return render_template("main_index.html")
+@app.route('/about')
+def about():
+    return render_template("child_about.html")
+@app.route('/zapis')
+def zapis():
+    return render_template("child_pages.html")
 
 if __name__ == '__main__':
     app.run()
